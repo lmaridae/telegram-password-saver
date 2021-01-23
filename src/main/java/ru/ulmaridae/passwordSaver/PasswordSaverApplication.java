d723a149bfdf5745028b48a66c8c295bff13536d
lebedev <d.lebedev@vbankcenter.ru>
2021-01-23 13:31:00 +0300

    Init commit.

---
+++ src/main/java/ru/ulmaridae/passwordSaver/PasswordSaverApplication.java
@@ -0,0 +1,12 @@
+package ru.ulmaridae.passwordSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordSaverApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/ulmaridae/passwordSaver/PasswordSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulmaridae.passwordSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
